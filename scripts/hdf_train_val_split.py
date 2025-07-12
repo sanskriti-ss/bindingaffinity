8e0a75c49b6fc9e4dccb36b411f4eaec4c329f88
Manvi-Agrawal <manvimeerut@gmail.com>
2025-07-12 22:09:23 +0530

    create, train, val and test data splits

--- scripts/hdf_train_val_split.py
+++ scripts/hdf_train_val_split.py
@@ -11,15 +11,17 @@ source_file = "pdbbind2016_core_test.hdf"
 
 # Load top-level protein group names
 with h5py.File(source_file, 'r') as src:
-    protein_ids = list(src.keys())[:150] # Change 10 to other number
+    protein_ids = list(src.keys())[:150] # Change 150 to other number
+    test_ids = list(src.keys())[150:200] 
 
-# Shuffle and split
+# # Shuffle and split
 random.shuffle(protein_ids)
 split_idx = int(len(protein_ids) * 0.8)
 train_ids = protein_ids[:split_idx]
-test_ids = protein_ids[split_idx:]
+val_ids = protein_ids[split_idx:]
 
 print(f"Train proteins: {train_ids}")
+print(f"Val proteins: {val_ids}")
 print(f"Test proteins: {test_ids}")
 
 
@@ -29,6 +31,7 @@ def copy_proteins(protein_list, dst_file):
         for pid in protein_list:
             src.copy(pid, dst)
 
-# Write train and test files
+# Write train, val and test files
 copy_proteins(train_ids, 'train.hdf')
-copy_proteins(test_ids, 'val.hdf')
+copy_proteins(val_ids, 'val.hdf')
+copy_proteins(test_ids, 'test.hdf')
